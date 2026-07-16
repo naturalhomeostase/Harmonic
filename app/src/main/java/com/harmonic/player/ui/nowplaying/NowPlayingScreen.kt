b419684984a207f5ff97473c265a3c69b2d20190
Renata <natural.homeostase@gmail.com>
2026-07-15 22:23:43 -0300

    Add files via upload

--- app/src/main/java/com/harmonic/player/ui/nowplaying/NowPlayingScreen.kt
+++ app/src/main/java/com/harmonic/player/ui/nowplaying/NowPlayingScreen.kt
@@ -1,6 +1,7 @@
 package com.harmonic.player.ui.nowplaying
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
