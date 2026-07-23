a51001086bce6b7d9a08443f757ce76f7044e3d3
Renata <natural.homeostase@gmail.com>
2026-07-23 18:10:17 -0300

    Add files via upload

--- app/src/main/java/com/harmonic/player/ui/playlists/PlaylistDetailScreen.kt
+++ app/src/main/java/com/harmonic/player/ui/playlists/PlaylistDetailScreen.kt
@@ -3,6 +3,7 @@ package com.harmonic.player.ui.playlists
 import android.content.Context
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
